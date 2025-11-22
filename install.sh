5599ecbc9bd93dbb8df00f5d9211e8277c517ddf
Emmet <emmet_king@hotmail.com>
2025-11-22 17:55:08 +0100

    experiment

--- install.sh
+++ install.sh
@@ -1,55 +1,50 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
-echo "Updating package index..."
-sudo apt-get update
+# Update and upgrade system packages
+echo "Updating system..."
+sudo apt-get update -y
 
+# Install necessary packages
 echo "Installing required packages..."
-sudo apt-get install -y usb-modeswitch git build-essential ca-certificates
+sudo apt-get install -y usb-modeswitch
 
-# --------------------------------------------------------------------
-# Install 3proxy only if missing
-# --------------------------------------------------------------------
-if ! command -v 3proxy >/dev/null 2>&1; then
-  echo "Building 3proxy..."
-  git clone https://github.com/z3apa3a/3proxy /tmp/3proxy-src
-  cd /tmp/3proxy-src
-  ln -sf Makefile.Linux Makefile
-  make -j$(nproc)
-  sudo make install
-  cd -
-  rm -rf /tmp/3proxy-src
-else
-  echo "3proxy already installed."
-fi
+echo "Installing 3proxy..."
+git clone https://github.com/z3apa3a/3proxy /tmp/3proxy-src
+cd /tmp/3proxy-src
+ln -sf Makefile.Linux Makefile
+make -j$(nproc)
+sudo make install
+cd -
+sleep 2
 
-echo "Installing configuration files..."
+###########################
+# Disable default 3proxy service
+###########################
+echo "Disabling system 3proxy service..."
+sudo systemctl stop 3proxy.service 2>/dev/null
+sudo systemctl disable 3proxy.service 2>/dev/null
 
-# Udev rules
-sudo install -m644 udev/40-3566-switch.rules /etc/udev/rules.d/40-3566-switch.rules
-sudo install -m644 udev/70-modem-netnames.rules /etc/udev/rules.d/70-modem-netnames.rules
+# Remove default config so systemd cannot start it even by accident
+sudo rm -f /etc/3proxy/3proxy.cfg
 
-# Scripts
-sudo install -m755 scripts/modeswitch-wrapper.sh /usr/local/bin/modeswitch-wrapper.sh
-sudo install -m755 scripts/modem-init.sh /usr/local/bin/modem-init.sh
+###########################
 
-# MAC definitions
-sudo install -m644 modem_macs.conf /etc/modem_macs.conf
-
-# 3proxy config
+# Setup 3proxy custom config
+echo "Setting up 3proxy..."
 sudo install -m644 3proxy.cfg /home/pi/3proxy.cfg
 sudo chown pi:pi /home/pi/3proxy.cfg
 
-# Systemd services
-sudo install -m644 systemd/modem-init.service /etc/systemd/system/modem-init.service
-sudo install -m644 systemd/3proxy.service /etc/systemd/system/3proxy.service
+# Setup the startproxy.sh script
+echo "Creating startup script..."
+sudo cp ./startproxy.sh /home/pi/startproxy.sh
+sudo chmod +x /home/pi/startproxy.sh
 
-sudo systemctl daemon-reload
-sudo systemctl enable --now modem-init.service
-sudo systemctl enable --now 3proxy.service
+# Configure udev rules for Huawei dongle
+echo "Setting up udev rules..."
+sudo cp ./40-huawei.rules /etc/udev/rules.d/40-huawei.rules
 
-echo "Reloading udev rules..."
+cudo usb_modeswitch -v 3566 -p 2001 -X
+
 sudo udevadm control --reload-rules
 
-echo "Installation complete. Reboot recommended."
-
+echo "Installation complete! Reboot system."
---
+++ startproxy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting 3proxy..."
+sudo 3proxy /home/pi/3proxy.cfg
